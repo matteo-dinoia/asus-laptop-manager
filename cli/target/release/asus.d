4e25e4ef5cce107b87022a90e5e2f218213f637a
Matteo Di Noia <dmatteo002@gmail.com>
2025-05-13 16:25:44 +0200

    remove target

--- cli/target/release/asus.d
+++
@@ -1,1 +0,0 @@
-/home/matteo/Documents/asus-laptop-manager/target/release/asus: /home/matteo/Documents/asus-laptop-manager/src/main.rs
--- cli/target/release/deps/asus-5347fa909f4a6a27.d
+++
@@ -1,5 +0,0 @@
-/home/matteo/Documents/asus-laptop-manager/target/release/deps/asus-5347fa909f4a6a27: src/main.rs
-
-/home/matteo/Documents/asus-laptop-manager/target/release/deps/asus-5347fa909f4a6a27.d: src/main.rs
-
-src/main.rs:
